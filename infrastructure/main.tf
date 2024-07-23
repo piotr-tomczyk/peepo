7bd727584d2472541d27d31742bb1901ae552f4d
Piotr Tomczyk <piotr.tomczyk@appfire.com>
2024-07-23 19:01:16 +0200

    Create compute instance

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,23 +10,23 @@ provider "google" {
   region = var.gcp_region
 }
 
-# resource "google_compute_instance" "default" {
-#   name         = "peepo-vm"
-#   machine_type = "e2-micro"
-#   zone         = "us-central1-a"
-#   metadata_startup_script = file("../startup.sh")
-#
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-12-bookworm-v20240709"
-#     }
-#   }
-#
-#   network_interface {
-#     network = "default"
-#     access_config {}
-#   }
-# }
+resource "google_compute_instance" "default" {
+  name         = "peepo-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  metadata_startup_script = file("../startup.sh")
+
+  boot_disk {
+    initialize_params {
+      image = "debian-12-bookworm-v20240709"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
 
 resource "google_secret_manager_secret" "discord-token" {
   secret_id = "discord-token"
