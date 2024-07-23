fef56535d1a9c1dc2777f0ab58f459cdc6aec196
Piotr Tomczyk <piotr.tomczyk@appfire.com>
2024-07-23 18:18:52 +0200

    Add env variables to the script

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "gcs" {
     bucket  = "terraform-state-peepo-bot"
+    prefix = "terraform/state"
   }
 }
 
@@ -25,4 +26,28 @@ resource "google_compute_instance" "default" {
     network = "default"
     access_config {}
   }
+}
+
+resource "google_secret_manager_secret" "discord-token" {
+  secret_id = "discord-token"
+}
+
+resource "google_secret_manager_secret" "openapi-token" {
+  secret_id = "openapi-token"
+}
+
+resource "google_secret_manager_secret" "tenorapi-token" {
+  secret_id = "tenorapi-token"
+}
+
+resource "google_secret_manager_secret" "textchannel-id" {
+  secret_id = "textchannel-id"
+}
+
+resource "google_secret_manager_secret" "gifchannel-id" {
+  secret_id = "gifchannel-id"
+}
+
+resource "google_secret_manager_secret" "guild-id" {
+  secret_id = "guild-id"
 }
--- startup.sh
+++ startup.sh
@@ -6,4 +6,13 @@ cd peepo
 git checkout peepo-terraforming
 pnpm i pm2@latest -g
 pnpm i
+touch .env
+cat > .env <<EOF
+DISCORD_TOKEN="gcloud compute secrets access latest --secret discord-token"
+OPENAI_API_KEY="gcloud compute secrets access latest --secret openapi-token"
+TENOR_API_KEY="gcloud compute secrets access latest --secret tenorapi-token"
+TEXT_CHANNEL_ID="gcloud compute secrets access latest --secret textchannel-id"
+GIF_CHANNEL_ID="gcloud compute secrets access latest --secret gifchannel-id"
+GUILD_ID="gcloud compute secrets access latest --secret guild-id"
+EOF
 pnpm start
