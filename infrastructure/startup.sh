a27c9919602c655300b0dda29a63bcbd1adba6fd
Piotr Tomczyk <piotr.tomczyk@appfire.com>
2024-07-23 19:22:17 +0200

    Create compute instance

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 sudo apt-get install git
-git clone https://github.com/piotr-tomczyk/peepo
+git clone https://github.com/piotr-tomczyk/peepo.git
 cd peepo
 git checkout peepo-terraforming
+nvm install 20.15.1
+nvm install node
 pnpm i pm2@latest -g
 pnpm i
-touch .env
-cat > .env <<EOF
-DISCORD_TOKEN="gcloud compute secrets access latest --secret discord-token"
-OPENAI_API_KEY="gcloud compute secrets access latest --secret openapi-token"
-TENOR_API_KEY="gcloud compute secrets access latest --secret tenorapi-token"
-TEXT_CHANNEL_ID="gcloud compute secrets access latest --secret textchannel-id"
-GIF_CHANNEL_ID="gcloud compute secrets access latest --secret gifchannel-id"
-GUILD_ID="gcloud compute secrets access latest --secret guild-id"
-EOF
+export DISCORD_TOKEN="$(gcloud secrets versions access latest --secret="discord-token")"
+export OPENAI_API_KEY="$(gcloud secrets versions access latest --secret="openapi-token")"
+export TENOR_API_KEY="$(gcloud secrets versions access latest --secret="tenorapi-token")"
+export TEXT_CHANNEL_ID="$(gcloud secrets versions access latest --secret="textchannel-id")"
+export GIF_CHANNEL_ID="$(gcloud secrets versions access latest --secret="gifchannel-id")"
+export GIF_CHANNEL_ID="$(gcloud secrets versions access latest --secret="gifchannel-id")"
+export GUILD_ID="$(gcloud secrets versions access latest --secret="guild-id")"
 pnpm start
